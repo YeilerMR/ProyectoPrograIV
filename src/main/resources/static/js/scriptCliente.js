24316178a8fb6383ca1716e2efda1b7eb63cdc50
Aaron <Aaron@Laptop>
2024-05-10 09:20:15 -0600

    CAMBIOS EN MODULO CLIENTE

--- src/main/resources/static/js/scriptCliente.js
+++ src/main/resources/static/js/scriptCliente.js
@@ -1,5 +1,5 @@
 // Función PopUp Crear Nuevo Cliente
-document.addEventListener("DOMContentLoaded", function () {
+function popupCrearCliente() {
     var modalCrear = document.getElementById("myModalCrearCliente");
     var btnAgregar = document.querySelector(".add_new");
     var spanCrear = document.querySelector("#myModalCrearCliente .close");
@@ -17,10 +17,10 @@ document.addEventListener("DOMContentLoaded", function () {
             modalCrear.style.display = "none";
         }
     };
-});
+}
 
 // Función PopUp Actualizar Cliente
-document.addEventListener("DOMContentLoaded", function () {
+function popupActualizarCliente() {
     var editButtons = document.querySelectorAll('.producto-editar');
     var closeButtons = document.querySelectorAll('.modal .close');
 
@@ -39,10 +39,10 @@ document.addEventListener("DOMContentLoaded", function () {
             popup.style.display = 'none';
         };
     });
-});
+}
 
 // Función Ver Contraseña en Tabla 
-document.addEventListener("DOMContentLoaded", function () {
+function togglePasswordInTable() {
     const toggleButtons = document.querySelectorAll('.toggle-password');
 
     toggleButtons.forEach(button => {
@@ -65,10 +65,10 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
-});
+}
 
 // Función Ver Contraseña en PopUp (Editar-Crear) 
-document.addEventListener("DOMContentLoaded", function () {
+function togglePasswordInPopup() {
     const toggleButtons = document.querySelectorAll('.toggle-password');
 
     toggleButtons.forEach(button => {
@@ -88,46 +88,63 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
-});
+}
 
+// Función para manejar la edición de cliente
+function handleClientEdit() {
+    // Obtener todos los botones de editar
+    const botonesEditar = document.querySelectorAll('.producto-editar');
 
-// Obtener todos los botones de editar
-const botonesEditar = document.querySelectorAll('.producto-editar');
+    // Agregar un evento click a cada botón de editar
+    botonesEditar.forEach(boton => {
+        boton.addEventListener('click', () => {
+            // Obtener los datos del cliente desde los atributos data-*
+            const idCliente = boton.closest('tr').querySelector('.producto-eliminar a').href.split('=')[1];
+            const nombre = boton.closest('tr').querySelector('td:nth-child(1)').textContent;
+            const apellidos = boton.closest('tr').querySelector('td:nth-child(2)').textContent;
+            const cedula = boton.closest('tr').querySelector('td:nth-child(3)').textContent;
+            const email = boton.closest('tr').querySelector('td:nth-child(4)').textContent;
+            const password = boton.closest('tr').querySelector('td:nth-child(5) .password').dataset.password;
+            const telefono = boton.closest('tr').querySelector('td:nth-child(6)').textContent;
+            const credencial = boton.closest('tr').querySelector('td:nth-child(7)').textContent === 'Cliente' ? 0 : 1;
 
-// Agregar un evento click a cada botón de editar
-botonesEditar.forEach(boton => {
-    boton.addEventListener('click', () => {
-        // Obtener los datos del cliente desde los atributos data-*
-        const idCliente = boton.closest('tr').querySelector('.producto-eliminar a').href.split('=')[1];
-        const nombre = boton.closest('tr').querySelector('td:nth-child(1)').textContent;
-        const apellidos = boton.closest('tr').querySelector('td:nth-child(2)').textContent;
-        const cedula = boton.closest('tr').querySelector('td:nth-child(3)').textContent;
-        const email = boton.closest('tr').querySelector('td:nth-child(4)').textContent;
-        const password = boton.closest('tr').querySelector('td:nth-child(5) .password').dataset.password;
-        const telefono = boton.closest('tr').querySelector('td:nth-child(6)').textContent;
-        const credencial = boton.closest('tr').querySelector('td:nth-child(7)').textContent === 'Cliente' ? 0 : 1;
+            // Rellenar los campos del formulario con los datos del cliente
+            document.querySelector('#modalEditar #idCliente').value = idCliente;
+            document.querySelector('#modalEditar input[name="nombre"]').value = nombre;
+            document.querySelector('#modalEditar input[name="apellidos"]').value = apellidos;
+            document.querySelector('#modalEditar input[name="cedula"]').value = cedula;
+            document.querySelector('#modalEditar input[name="email"]').value = email;
+            document.querySelector('#modalEditar input[name="password"]').value = password;
+            document.querySelector('#modalEditar input[name="telefono"]').value = telefono;
+            document.querySelector('#modalEditar select[name="credencial"]').value = credencial;
 
-        // Rellenar los campos del formulario con los datos del cliente
-        document.querySelector('#modalEditar #idCliente').value = idCliente;
-        document.querySelector('#modalEditar input[name="nombre"]').value = nombre;
-        document.querySelector('#modalEditar input[name="apellidos"]').value = apellidos;
-        document.querySelector('#modalEditar input[name="cedula"]').value = cedula;
-        document.querySelector('#modalEditar input[name="email"]').value = email;
-        document.querySelector('#modalEditar input[name="password"]').value = password;
-        document.querySelector('#modalEditar input[name="telefono"]').value = telefono;
-        document.querySelector('#modalEditar select[name="credencial"]').value = credencial;
-
-        // Mostrar el modal de edición
-        document.querySelector('#modalEditar').style.display = 'block';
-    });
-});
-
-// Código para cerrar el modal cuando se hace clic en el botón de cerrar
-const spanCerrar = document.querySelectorAll('.close');
-spanCerrar.forEach(span => {
-    span.addEventListener('click', () => {
-        document.querySelectorAll('.modal').forEach(modal => {
-            modal.style.display = 'none';
+            // Mostrar el modal de edición
+            document.querySelector('#modalEditar').style.display = 'block';
         });
     });
+}
+
+// Función para cerrar los modales
+function closeModal() {
+    const spanCerrar = document.querySelectorAll('.close');
+    spanCerrar.forEach(span => {
+        span.addEventListener('click', () => {
+            document.querySelectorAll('.modal').forEach(modal => {
+                modal.style.display = 'none';
+            });
+        });
+    });
+}
+
+// Llamadas a las funciones
+document.addEventListener("DOMContentLoaded", function () {
+    validarEliminacion('.producto-eliminar', 'Cliente eliminado exitosamente');
+    validarEdicion('.editar-cliente-form', '¿Estás seguro de continuar con la edición de este cliente?', '/clientes/listar');
+    validarCreacion('.crear-cliente-form', '¿Estás seguro de continuar con la creación de este cliente?', '/clientes/listar');
+    popupCrearCliente();
+    popupActualizarCliente();
+    togglePasswordInTable();
+    togglePasswordInPopup();
+    handleClientEdit();
+    closeModal();
 });
